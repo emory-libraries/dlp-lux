1c6452029be97ac3ee53ec57b31c97a5c1dec496
Solomon Hilliard <solomon.hilliard@emory.edu>
2019-11-22 10:04:10 -0500

    Setup capistrano (#1)
    * Update git ignore and Gemfile
    
    * Configure deploy targets
    
    * Add env vars to database config
    
    * Add mysql gem
    
    * Attempt to fix uglyfier error
    
    * Adjust application name
    
    * Adjust app name and deploy path
    
    * Rubocop fix
    
    * Add arch environment and shortcuts

---
+++ config/deploy/test.rb
@@ -0,0 +1,9 @@
+set :stage, :TEST
+ec2_role %i[web app db],
+         user: 'deploy',
+         ssh_options: {
+           keys: ENV['SSH_EC2_KEY_FILE'],
+           forward_agent: true,
+           verify_host_key: :never
+         }
+# server 'PRIVATE_IP_Address', user: 'deploy', roles: %i[web app db]
