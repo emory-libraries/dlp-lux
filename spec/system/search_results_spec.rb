3819339ebe47e3e4ca4dac674f010bcd47fc2db1
Bess Sadler <bess@curationexperts.com>
2019-12-03 11:58:36 -0500

    Include a solr snapshot and a way to test against it

---
+++ spec/system/search_results_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+require 'rails_helper'
+include Warden::Test::Helpers
+
+RSpec.describe 'front page', type: :system, js: true do
+  let(:solr_snaphots_location) { "#{fixture_path}/solr_snapshots/" }
+  let(:solr_snaphot_name) { "20191203163502341" }
+
+  before do
+    `curl "http://localhost:8985/solr/hydra-test/replication?command=restore&location=#{solr_snaphots_location}&name=#{solr_snaphot_name}"`
+  end
+
+  it 'can load snapshot index' do
+    visit "/?search_field=all_fields&q="
+    expect(page).to have_content '1 - 10 of 54,797'
+  end
+end
