d7d4799cdc0b583f45d39d2452221d486e484679
Max Kadel <max@curationexperts.com>
2019-12-03 17:06:43 -0500

    Rubocop fixes

--- app/helpers/lux/catalog_helper.rb
+++ app/helpers/lux/catalog_helper.rb
@@ -4,12 +4,12 @@ module Lux
     # @param [Hash] args from get_field_values
     def render_human_readable_date(args)
       date_created = args[:value]
-      date_created.map{ |date_created| human_readable_date(date_created) }.join( ', ')
+      date_created.map { |date| human_readable_date(date) }.join(', ')
     end
 
     def human_readable_date(date_created)
       return 'unknown' if date_created == 'XXXX'
-      return date_created.gsub('?','0s') if date_created.size == 4 && date_created.ends_with?('?')
+      return date_created.gsub('?', '0s') if date_created.size == 4 && date_created.ends_with?('?')
       date_created
     end
   end
