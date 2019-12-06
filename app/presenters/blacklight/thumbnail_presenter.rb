e45a2ca6307fc1be428f5e65ebdc1e096b4768e1
Max Kadel <max@curationexperts.com>
2019-12-06 17:17:16 -0500

    Embed _thumbnail partial in _index partial for search results

--- app/presenters/blacklight/thumbnail_presenter.rb
+++ app/presenters/blacklight/thumbnail_presenter.rb
@@ -31,7 +31,7 @@ module Blacklight
     # @param [Hash] image_options to pass to the image tag
     # @param [Hash] url_options to pass to #link_to_document
     # @return [String]
-    def thumbnail_tag image_options = {}, url_options = {}
+    def thumbnail_tag(image_options = {}, url_options = {})
       value = thumbnail_value(image_options)
       return value if value.nil? || url_options[:suppress_link]
 
@@ -40,46 +40,47 @@ module Blacklight
 
     private
 
-    delegate :thumbnail_field, :thumbnail_method, :default_thumbnail, to: :view_config
+      delegate :thumbnail_field, :thumbnail_method, :default_thumbnail, to: :view_config
 
-    # @param [Hash] image_options to pass to the image tag
-    def thumbnail_value(image_options)
-      value = if thumbnail_method
-                view_context.send(thumbnail_method, document, image_options)
-              elsif thumbnail_field
-                image_url = ENV['THUMBNAIL_URL'] + thumbnail_value_from_document
-                image_options.merge!({alt: "Thumbnail image"})
-                view_context.image_tag image_url, image_options if image_url.present?
-              end
+      # @param [Hash] image_options to pass to the image tag
+      def thumbnail_value(image_options)
+        value = if thumbnail_method
+                  view_context.send(thumbnail_method, document, image_options)
+                elsif thumbnail_field
+                  image_url = (ENV['THUMBNAIL_URL'] || '') + thumbnail_value_from_document
+                  image_options[:alt] = "Thumbnail image"
+                  image_options[:class] = "img-fluid"
+                  view_context.image_tag image_url, image_options if image_url.present?
+                end
 
-      value || default_thumbnail_value(image_options)
-    end
-
-    def default_thumbnail_value(image_options)
-      return unless default_thumbnail
-
-      case default_thumbnail
-      when Symbol
-        view_context.send(default_thumbnail, document, image_options)
-      when Proc
-        default_thumbnail.call(document, image_options)
-      else
-        view_context.image_tag default_thumbnail, image_options
+        value || default_thumbnail_value(image_options)
       end
-    end
 
-    def thumbnail_value_from_document
-      Array(thumbnail_field).lazy.map { |field| retrieve_values(field_config(field)).first }.reject(&:blank?).first
-    end
+      def default_thumbnail_value(image_options)
+        return unless default_thumbnail
 
-    def retrieve_values(field_config)
-      FieldRetriever.new(document, field_config).fetch
-    end
+        case default_thumbnail
+        when Symbol
+          view_context.send(default_thumbnail, document, image_options)
+        when Proc
+          default_thumbnail.call(document, image_options)
+        else
+          view_context.image_tag default_thumbnail, image_options
+        end
+      end
 
-    def field_config(field)
-      return field if field.is_a? Blacklight::Configuration::Field
+      def thumbnail_value_from_document
+        Array(thumbnail_field).lazy.map { |field| retrieve_values(field_config(field)).first }.reject(&:blank?).first
+      end
 
-      Configuration::NullField.new(field)
-    end
+      def retrieve_values(field_config)
+        FieldRetriever.new(document, field_config).fetch
+      end
+
+      def field_config(field)
+        return field if field.is_a? Blacklight::Configuration::Field
+
+        Configuration::NullField.new(field)
+      end
   end
 end
