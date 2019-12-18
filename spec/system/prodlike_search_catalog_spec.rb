f1f68c1b4668923830590c36e3c38b1605521627
Max Kadel <max@curationexperts.com>
2019-12-18 13:32:47 -0500

    Rubocop

--- spec/system/prodlike_search_catalog_spec.rb
+++ spec/system/prodlike_search_catalog_spec.rb
@@ -3,9 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Search a prod-like catalog', type: :system, run_in_ci: false, relevancy: true, js: true do
-
   it 'gets correct search results' do
-
     # Do we need to make sure any previously opened connections have closed?
     visit 'https://digital-arch.library.emory.edu:443'
     # Search for something
