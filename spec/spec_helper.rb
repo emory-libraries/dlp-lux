937935f9bbc65b58708fec5722bd9cad9ef4bcec
Max Kadel <max@curationexperts.com>
2019-12-18 16:39:29 -0500

    Filter out /spec folders from coverage reports

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/' # for rspec
+end
+
 require 'coveralls'
 Coveralls.wear!
 
