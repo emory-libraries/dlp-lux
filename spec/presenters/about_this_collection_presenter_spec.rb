6b949e3d2b30cdd342cf3a244b7f432ceb91b448
Max Kadel <max@curationexperts.com>
2020-01-16 14:36:41 -0500

    Collection page redux (#239)
    * cherry-pick bd3539a
    
    * cherry-pick c7d72cc
    
    * WIP: link to objects in collection
    
    * Preliminary work on view collection page done
    
    - Shows link to items within digital collection in partial
    - Has a separate "About this collection" partial

---
+++ spec/presenters/about_this_collection_presenter_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe AboutThisCollectionPresenter do
+  let(:pres) { described_class.new(document: COLLECTION) }
+  let(:about_terms) do
+    { abstract_tesim: ["Collection of yellowbacks and paperbacks collected by Chester W. Topp.  Yellowbacks are cheap novels published in the late nineteenth and early twentieth century in Great Britain."],
+      creator_tesim: ["Topp, Chester W., collector."],
+      holding_repository_tesim: ["Stuart A. Rose Manuscript, Archives, and Rare Book Library"] }
+  end
+  context 'with a solr document' do
+    describe '#terms' do
+      it 'has the correct terms' do
+        expect(pres.terms).to eq(about_terms)
+      end
+    end
+  end
+end
