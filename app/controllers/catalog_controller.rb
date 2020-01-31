060149afcba346c8dacd673e66d704250a76b1ec
Will Ulman <william.ulman@emory.edu>
2020-01-31 09:54:52 -0500

    Add case-insensitive sorting

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -469,10 +469,10 @@ class CatalogController < ApplicationController
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc, year_for_lux_isim desc, title_ssi asc', label: 'relevance'
-    config.add_sort_field 'year_for_lux_isim desc, title_ssi asc', label: 'year'
-    config.add_sort_field 'creator_ssi asc', label: 'creator'
-    config.add_sort_field 'title_ssi asc, year_for_lux_isim desc', label: 'title'
+    config.add_sort_field 'score desc, year_for_lux_isim desc, title_ssort asc', label: 'relevance'
+    config.add_sort_field 'year_for_lux_isim desc, title_ssort asc', label: 'year'
+    config.add_sort_field 'creator_ssort asc', label: 'creator'
+    config.add_sort_field 'title_ssort asc, year_for_lux_isim desc', label: 'title'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
