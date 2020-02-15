d32f23bf267a2d7c22d78bf77e3f72e202308a37
Max Kadel <max@curationexperts.com>
2020-02-15 17:06:15 -0500

    Remove unused facet

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -22,7 +22,6 @@ RSpec.describe CatalogController, type: :controller do
        'subject_names',
        'subject_geo',
        'human_readable_rights_statement',
-       'visibility_ssi',
        'access_pivot_facet']
     end
 
