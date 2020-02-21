be83ffbec8a27a65d09df120866b800d9ec07d86
Will Ulman <william.ulman@emory.edu>
2020-02-21 09:32:37 -0500

    Expand test coverage

--- spec/system/faceted_search_spec.rb
+++ spec/system/faceted_search_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 
 RSpec.describe 'Search the catalog by facets', type: :system, js: false do
   it 'displays correct labels in constraint elements' do
+    visit '/?f%5Bholding_repository_sim%5D%5B%5D=Sample+Library'
+    expect(page).to have_css('.filter-name', text: /\ALibrary\z/)
+
+    visit '/?f%5Bmember_of_collections_ssim%5D%5B%5D=Sample+Collection'
+    expect(page).to have_css('.filter-name', text: /\ACollection\z/)
+
     visit '/?f%5Bcreator_sim%5D%5B%5D=Sample+Creator'
     expect(page).to have_css('.filter-name', text: /\ACreator\z/)
-
-    visit '/?f%5Bcontributors_sim%5D%5B%5D=Sample+Contributor'
-    expect(page).to have_css('.filter-name', text: /\AContributors\z/)
 
     visit '/?f%5Bhuman_readable_content_type_ssim%5D%5B%5D=Sample+Format'
     expect(page).to have_css('.filter-name', text: /\AFormat\z/)
@@ -27,10 +30,16 @@ RSpec.describe 'Search the catalog by facets', type: :system, js: false do
     visit '/?f%5Bsubject_geo_sim%5D%5B%5D=Sample+Geo'
     expect(page).to have_css('.filter-name', text: /\ASubject - Geographic Locations\z/)
 
+    visit '/?f%5Bhuman_readable_rights_statement_ssim%5D%5B%5D=Sample+Rights'
+    expect(page).to have_css('.filter-name', text: /\ARights Status\z/)
+
+    visit '/?f%5Bvisibility_group_ssi%5D%5B%5D=Sample+Access'
+    expect(page).to have_css('.filter-name', text: /\AAccess\z/)
+
+    visit '/?f%5Bcontributors_sim%5D%5B%5D=Sample+Contributor'
+    expect(page).to have_css('.filter-name', text: /\AContributors\z/)
+
     visit '/?f%5Bkeywords_sim%5D%5B%5D=Sample+Keywords'
     expect(page).to have_css('.filter-name', text: /\AKeywords\z/)
-
-    visit '/?f%5Bholding_repository_sim%5D%5B%5D=Sample+Library'
-    expect(page).to have_css('.filter-name', text: /\ALibrary\z/)
   end
 end
