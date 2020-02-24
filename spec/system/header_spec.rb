d474f745d030af46df6211266da68ba8df070a99
Max Kadel <max@curationexperts.com>
2020-02-24 17:19:45 -0500

    Zones for header, shield showing

--- spec/system/header_spec.rb
+++ spec/system/header_spec.rb
@@ -8,5 +8,9 @@ RSpec.describe 'header', type: :system do
     expect(page).to have_css ".navbar-brand"
     expect(page).to have_link "Advanced Search"
     expect(page).to have_css ".search-query-form"
+    expect(page).to have_css ".top-links"
+    expect(page).to have_css ".emory-util-about"
+    expect(page).to have_link "Home"
+    expect(page).to have_css ".big-nav"
   end
 end
