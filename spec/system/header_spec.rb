1ad8e90196211cdc434b1434086818bf5ed112ab
Max Kadel <max@curationexperts.com>
2020-02-26 10:27:25 -0500

    Emory logo (#365)
    * Zones for header, shield showing
    
    * SVG version of Emory logo in header
    
    * re-add specific css for test
    
    * Zones for header, shield showing
    
    * SVG version of Emory logo in header
    
    * Zones for header, shield showing
    
    * SVG version of Emory logo in header
    
    * Zones for header, shield showing
    
    * SVG version of Emory logo in header
    
    * Incremental header improvements
    
    * Changed CSS for this element
    
    * Make it so we can turn off JS for test, so it passes CI
    
    * Turn off JS for CI

--- spec/system/header_spec.rb
+++ spec/system/header_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe 'header', type: :system do
     expect(page).to have_link "Advanced Search"
     expect(page).to have_css ".search-query-form"
     expect(page).to have_css ".top-links"
-    expect(page).to have_css ".emory-util-about"
     expect(page).to have_link "Home"
     expect(page).to have_css ".big-nav"
   end
