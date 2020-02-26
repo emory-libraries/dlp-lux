701a7693ad7bcf594e579c098dd6fd090eea72b7
Max Kadel <max@curationexperts.com>
2020-02-26 10:11:59 -0500

    Turn off JS for CI

--- spec/system/search_results_visibility_spec.rb
+++ spec/system/search_results_visibility_spec.rb
@@ -2,7 +2,7 @@
 require 'rails_helper'
 include Warden::Test::Helpers
 
-RSpec.describe "View search results for works with different levels of visibility", js: true, clean: true, type: :system do
+RSpec.describe "View search results for works with different levels of visibility", js: false, clean: true, type: :system do
   before do
     delete_all_documents_from_solr
     solr = Blacklight.default_index.connection
@@ -16,7 +16,7 @@ RSpec.describe "View search results for works with different levels of visibilit
              ])
     solr.commit
     visit "/"
-    find('.submit-search-text').click
+    click_on('search')
   end
 
   let(:emory_high_work_id) { '111-321' }
