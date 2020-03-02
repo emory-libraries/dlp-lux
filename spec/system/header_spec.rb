04ef267386e01cd47c1f958349df410043aa6320
Max Kadel <max@curationexperts.com>
2020-03-02 15:23:35 -0500

    Replace ID for search for ease of testing, correct expectations for header

--- spec/system/header_spec.rb
+++ spec/system/header_spec.rb
@@ -5,11 +5,9 @@ include Warden::Test::Helpers
 RSpec.describe 'header', type: :system do
   it 'has expected css' do
     visit "/"
-    expect(page).to have_css ".navbar-brand"
+    expect(page).to have_css ".branding-header"
     expect(page).to have_link "Advanced Search"
     expect(page).to have_css ".search-query-form"
-    expect(page).to have_css ".top-links"
-    expect(page).to have_link "Home"
-    expect(page).to have_css ".big-nav"
+    expect(page).to have_link "Emory Libraries"
   end
 end
