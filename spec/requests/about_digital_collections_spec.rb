ead7caba426d6a7fe51d36ec92c9b4d17632b97e
bwatson78 <brad.watson.orlando@gmail.com>
2020-03-16 14:07:15 -0500

    Strips away unneeded structure and adds more tests. 1. _content_chunk.html.erb: strips away unneeded table structure. 2. about.html.erb: reformats the needed variables for the partial. 3. about_digital_collections_spec.rb: tests that all headers are present.

--- spec/requests/about_digital_collections_spec.rb
+++ spec/requests/about_digital_collections_spec.rb
@@ -12,9 +12,14 @@ RSpec.describe "About Digital Collections", type: :request do
     expect(response.content_type).to eq "text/html"
   end
 
-  it "contains the requested text" do
+  it "contains the requested headers" do
     get "/about"
 
     expect(response.body).to include 'About Emory Digital Collections'
+    expect(response.body).to include 'How to use Digital Collections'
+    expect(response.body).to include 'Technical Information'
+    expect(response.body).to include 'Copyright and Reuse'
+    expect(response.body).to include 'Contact'
+    expect(response.body).to include 'Donate'
   end
 end
