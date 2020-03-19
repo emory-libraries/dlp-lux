5ba6b4b8250bf55aebb2c5d9178d869571c1e1da
Max Kadel <max@curationexperts.com>
2020-03-19 09:59:40 -0400

    Update salts to environment variables

--- spec/requests/omniauth_callbacks_requests_spec.rb
+++ spec/requests/omniauth_callbacks_requests_spec.rb
@@ -29,8 +29,8 @@ RSpec.describe OmniauthCallbacksController, :clean, type: :request do
   end
 
   def decrypt_string(encrypted_str)
-    cipher_salt1 = 'some-random-salt-'
-    cipher_salt2 = 'another-random-salt-'
+    cipher_salt1 = ENV["IIIF_COOKIE_SALT_1"] || 'some-random-salt-'
+    cipher_salt2 = ENV["IIIF_COOKIE_SALT_2"] || 'another-random-salt-'
     cipher = OpenSSL::Cipher.new('AES-128-ECB').decrypt
     cipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(cipher_salt1, cipher_salt2, 20_000, cipher.key_len)
     decrypted = [encrypted_str].pack('H*').unpack('C*').pack('c*')
