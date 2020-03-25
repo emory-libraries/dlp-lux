f1c4fb758237e7dbd98de487f5368bc470535362
Max Kadel <max@curationexperts.com>
2020-03-25 09:11:41 -0400

    Refactor, rubocop

--- app/presenters/blacklight/thumbnail_presenter.rb
+++ app/presenters/blacklight/thumbnail_presenter.rb
@@ -57,19 +57,18 @@ module Blacklight
       def thumbnail_image_url
         visibility = document["visibility_ssi"]
         case visibility
-        when "open"
-          (ENV['THUMBNAIL_URL'] || '') + thumbnail_value_from_document
-        when "low_res"
-          (ENV['THUMBNAIL_URL'] || '') + thumbnail_value_from_document
-        when "emory_low"
+        when "open", "low_res"
+          lux_thumbnail_url
+        when "emory_low", "authenticated" # Authenticated is the same as Emory High Download
           return '/assets/login-required.png' unless view_context&.current_user
-          (ENV['THUMBNAIL_URL'] || '') + thumbnail_value_from_document
-        when "authenticated"
-          return '/assets/login-required.png' unless view_context&.current_user
-          (ENV['THUMBNAIL_URL'] || '') + thumbnail_value_from_document
+          lux_thumbnail_url
         when "rose_high"
-          return '/assets/reading-room-only.png' unless false
+          return '/assets/reading-room-only.png'
         end
+      end
+
+      def lux_thumbnail_url
+        (ENV['THUMBNAIL_URL'] || '') + thumbnail_value_from_document
       end
 
       def default_thumbnail_value(image_options)
