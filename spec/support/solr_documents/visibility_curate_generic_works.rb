2b6eb1566e7838f699a32cbb8812b749e41f06b2
Will Ulman <46227821+wulman16@users.noreply.github.com>
2020-03-25 12:19:41 -0400

    Revert "Thumbnails - Uses generic "Login" thumbnail for unauthenticated users (#438)" (#439)
    This reverts commit 6b6814e58e0fdc2be2a720840d58cb142d4ce06b.

--- spec/support/solr_documents/visibility_curate_generic_works.rb
+++ spec/support/solr_documents/visibility_curate_generic_works.rb
@@ -3,20 +3,17 @@ WORK_WITH_EMORY_HIGH_VISIBILITY = {
   id: '111-321',
   has_model_ssim: ['CurateGenericWork'],
   title_tesim: ['Work with Emory High visibility'],
-  thumbnail_path_ss: ['/downloads/111-321?file=thumbnail'],
   edit_access_group_ssim: ["admin"],
   read_access_group_ssim: ["registered"],
   visibility_ssi: ['authenticated'],
   visibility_group_ssi: "Log In Required",
   human_readable_visibility_ssi: "Emory High Download"
-
 }.freeze
 
 WORK_WITH_PUBLIC_VISIBILITY = {
   id: '222-321',
   has_model_ssim: ['CurateGenericWork'],
   title_tesim: ['Work with Open Access'],
-  thumbnail_path_ss: ['/downloads/222-321?file=thumbnail'],
   edit_access_group_ssim: ["admin"],
   read_access_group_ssim: ["public"],
   visibility_ssi: ['open'],
@@ -28,7 +25,6 @@ WORK_WITH_PUBLIC_LOW_VIEW_VISIBILITY = {
   id: '333-321',
   has_model_ssim: ['CurateGenericWork'],
   title_tesim: ['Work with Public Low Resolution'],
-  thumbnail_path_ss: ['/downloads/333-321?file=thumbnail'],
   edit_access_group_ssim: ["admin"],
   read_access_group_ssim: ["low_res"],
   visibility_ssi: ['low_res'],
@@ -41,7 +37,6 @@ WORK_WITH_EMORY_LOW_VISIBILITY = {
   id: '444-321',
   has_model_ssim: ['CurateGenericWork'],
   title_tesim: ['Work with Emory Low visibility'],
-  thumbnail_path_ss: ['/downloads/444-321?file=thumbnail'],
   edit_access_group_ssim: ["admin"],
   read_access_group_ssim: ["emory_low"],
   visibility_ssi: ["emory_low"],
@@ -53,7 +48,6 @@ WORK_WITH_ROSE_HIGH_VISIBILITY = {
   id: '555-321',
   has_model_ssim: ['CurateGenericWork'],
   title_tesim: ['Work with Rose High View visibility'],
-  thumbnail_path_ss: ['/downloads/555-321?file=thumbnail'],
   edit_access_group_ssim: ["admin"],
   read_access_group_ssim: ["rose_high"],
   visibility_ssi: ['rose_high'],
@@ -65,7 +59,6 @@ WORK_WITH_PRIVATE_VISIBILITY = {
   id: '666-321',
   has_model_ssim: ['CurateGenericWork'],
   title_tesim: ['Work with Private visibility'],
-  thumbnail_path_ss: ['/downloads/666-321?file=thumbnail'],
   edit_access_group_ssim: ["admin"],
   visibility_ssi: ["restricted"],
   human_readable_visibility_ssi: "Private"
