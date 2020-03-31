adbd2ec00fb82b5a253232bd82cd22bb76f73207
Max Kadel <max@curationexperts.com>
2020-03-31 16:09:22 -0400

    Searches for famous African Americans

--- spec/relevancy/basic_relevancy_spec.rb
+++ spec/relevancy/basic_relevancy_spec.rb
@@ -19,39 +19,82 @@ RSpec.describe 'Search a prod-like catalog', type: :system, run_in_ci: false, re
       "https://#{username}:#{password}@digital.library.emory.edu:443"
     end
   end
-
-  it 'gets correct search results' do
+  before do
     visit url
-    # Search for something
-    fill_in 'q', with: 'emory'
+    fill_in 'q', with: search_terms
     click_on 'search'
+  end
 
-    within '#documents' do
-      expect(page).to have_content('Emory University Yearbooks')
+  context "searches for emory" do
+    let(:search_terms) { 'emory' }
+
+    it 'gets correct search results' do
+      within '#documents' do
+        expect(page).to have_content('Emory University Yearbooks')
+      end
     end
   end
 
-  it "finds only a few results for 'barber dog'" do
-    visit url
-    # Search for something
-    fill_in 'q', with: 'barber dog'
-    click_on 'search'
-    expect(page.find(:xpath, '/html/body/main/div[3]/section[1]/div[1]/div[1]/span/strong[3]').text.to_i).to be_between(1, 5).inclusive
+  context "searches for 'barber dog'" do
+    let(:search_terms) { 'barber dog' }
+
+    it "finds only a few results" do
+      expect(results_number).to be_between(1, 5).inclusive
+    end
   end
 
-  it "finds many results for 'barber'" do
-    visit url
-    # Search for something
-    fill_in 'q', with: 'barber'
-    click_on 'search'
-    expect(/of\s(\d*)/.match(page.find('.page-entries').text)[1].to_i).to be > 50
+  context "searches for 'barber'" do
+    let(:search_terms) { 'barber' }
+
+    it "finds many results" do
+      expect(results_number).to be > 50
+    end
   end
 
-  it "finds at least 40 results for 'Jesse Jackson'" do
-    visit url
-    # Search for something
-    fill_in 'q', with: 'Jesse Jackson'
-    click_on 'search'
-    expect(page.find(:xpath, '/html/body/main/div[3]/section[1]/div[1]/div[1]/span/strong[3]').text.to_i).to be >= 40
+  context "searches for 'Jesse Jackson'" do
+    let(:search_terms) { 'Jesse Jackson' }
+
+    it "finds at least 40 results" do
+      expect(results_number).to be >= 40
+    end
+  end
+
+  context "searches for 'civil rights'" do
+    let(:search_terms) { 'civil rights' }
+
+    # This behavior should probably be changed
+    it "currently ranks full-text pamphlet above possibly more relevant objects when searching 'civil rights'" do
+      within '#documents' do
+        expect(page).to have_content('Joseph Poga_ny, the founder of the American Civil Righs Movement')
+      end
+    end
+
+    it "includes Martin Luther King, Jr. in the first page" do
+      within '#documents' do
+        expect(page).to have_content('Martin Luther King')
+      end
+    end
+  end
+
+  context "searches for 'Paul Robeson'" do
+    let(:search_terms) { 'Paul Robeson' }
+    let(:work_title) { 'Alberta Hunter and Paul Robeson on stage' }
+
+    it "ranks images where Paul Robeson is the subject of the photo above other relationships to Robeson" do
+      within '#documents' do
+        expect(page).to have_content('Alberta Hunter and Paul Robeson on stage')
+        expect(rank_in_results_title).to be >= 1
+        expect(rank_in_results_title).to be <= 5
+      end
+    end
+  end
+
+  def results_number
+    # Finds "of " and a number
+    /of\s(\d*)/.match(page.find('.page-entries').text)[1].to_i
+  end
+
+  def rank_in_results_title
+    page.find(".document-title-heading", text:work_title).find(".document-counter").text.to_i
   end
 end
