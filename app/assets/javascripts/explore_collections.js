b277c7b10d0b66f950103bc3e29fa1a531751050
Will Ulman <46227821+wulman16@users.noreply.github.com>
2020-04-09 12:34:12 -0400

    Add featured collection cards (#484)
    * Initial pass at content
    
    * Apply initial EUL styles
    
    * Refactor cards using presenter
    
    * Update front page spec
    
    * Fix heading color and card heights

---
+++ app/assets/javascripts/explore_collections.js
@@ -0,0 +1,28 @@
+$(document).on('turbolinks:load', function() {
+  setExploreCollectionsHeight()
+  $(window).on('resize', setExploreCollectionsHeight)
+})
+
+function setExploreCollectionsHeight() {
+  if (desktopWidth(window)) {
+    matchFacetsHeight()
+  } else {
+    setDefaultHeight()
+  }
+}
+
+function desktopWidth(w) {
+  return w.matchMedia("(min-width: 992px)").matches
+}
+
+function matchFacetsHeight() {
+  if ($('#facet-panel-collapse').height() < 560) {
+    setDefaultHeight()
+  } else {
+    return $('.tile-listing').css("height", $('#facet-panel-collapse').height())
+  }
+}
+
+function setDefaultHeight() {
+  return $('.tile-listing').css("height", "35rem")
+}
