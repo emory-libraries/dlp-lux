f080a6a2ede518c96b2471e93ab357048d005a6e
bwatson78 <brad.watson.orlando@gmail.com>
2020-04-15 09:33:14 -0500

    Final push

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  get 'errors/not_found'
-
   concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
   mount Blacklight::Engine => '/'
   mount BlacklightAdvancedSearch::Engine => '/'
---
+++ spec/requests/not_found_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe "404 Error Custom Page", type: :request do
+  it "loads the page when 404 error detected" do
+    get "/1q"
+
+    expect(response.status).to eq 404
+    expect(response.body).not_to be_empty
+    expect(response.content_length).to be > 0
+    expect(response.content_type).to eq "text/html"
+    expect(response).to render_template(:not_found)
+  end
+
+  it "contains the requested headers" do
+    get "/1q"
+
+    expect(response.body).to include 'Page Not Available'
+    expect(response.body).to include 'Accessing Emory Digital Collections'
+    expect(response.body).to include 'mailto:rose.library@emory.edu'
+    expect(response.body).to include 'our wiki site'
+    expect(response.body).to include 'https://wiki.service.emory.edu/display/DLPP/Emory+Digital+Collections+User+Guide'
+  end
+end
