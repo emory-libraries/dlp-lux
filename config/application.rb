3f1d0fd89a0e64c223a0e5f125774b1ebc6bfe5c
Brad Watson <brad.watson.orlando@gmail.com>
2020-04-15 13:59:21 -0500

    Ensures proper display of custom 404 page (#498)

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module LuxBl7
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.exceptions_app = routes
   end
 end
