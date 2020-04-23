2f01d8a116e6332ea14c77a8a0c33ce6272772c0
Will Ulman <will.ulman@gmail.com>
2020-04-23 11:49:52 -0400

    Do not pass in unneeded interval option

--- app/assets/javascripts/hero_image.js
+++ app/assets/javascripts/hero_image.js
@@ -1,5 +1,5 @@
 // Retrigger the hero image carousel after navigating the site
 // through turbolinks
 $(document).on('turbolinks:load', function() {
-  $('.carousel').carousel({ interval: 10000 });
+  $('.carousel').carousel();
 });
