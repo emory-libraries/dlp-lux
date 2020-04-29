ec7c5699b9473fdefae403156b27892cfa63356b
bwatson78 <brad.watson.orlando@gmail.com>
2020-04-29 11:16:41 -0500

    Refactors to place purl into presenter class and fixes hardcoding the href.

--- app/presenters/find_this_item_presenter.rb
+++ app/presenters/find_this_item_presenter.rb
@@ -3,13 +3,18 @@
 class FindThisItemPresenter
   attr_reader :document, :config
 
-  def initialize(document:)
+  def initialize(document:, solr_document:)
     @document = document
     @config = YAML.safe_load(File.open(Rails.root.join('config', 'metadata', 'find_this_item.yml')))
+    @solr_document = solr_document
   end
 
   def terms
     @config = @config.symbolize_keys
     @document.slice(*@config.keys)
+  end
+
+  def purl
+    "https://digital.library.emory.edu/purl/#{@solr_document[:id]}"
   end
 end
