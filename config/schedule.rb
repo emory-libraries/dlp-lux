0adcb01e9c122a5bda3ef3b31575e79f00fd64e3
Max Kadel <max@curationexperts.com>
2020-04-29 10:58:02 -0400

    set env for deploy for cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 # Use this file to easily define all of your cron jobs.
 #
+env :BASH_ENV, '/home/deploy/.bashrc'
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
