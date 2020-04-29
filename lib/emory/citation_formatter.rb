4886542c6d068013843273757f5f71e57ca87e13
bwatson78 <brad.watson.orlando@gmail.com>
2020-04-29 09:12:40 -0500

    Refactors the two modules to be explanatory with method names.

--- lib/emory/citation_formatter.rb
+++ lib/emory/citation_formatter.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'citeproc'
+require 'csl/styles'
 require './lib/emory/citation_string_processor'
 
 module Emory
@@ -9,7 +10,11 @@ module Emory
 
     def initialize(obj)
       @obj = obj
-      @default_citations = { "apa": apa_default, "chicago-fullnote-bibliography": chicago_default, "modern-language-association": mla_default }
+      @default_citations = {
+        "apa": apa_default_citation,
+        "chicago-fullnote-bibliography": chicago_default_citation,
+        "modern-language-association": mla_default_citation
+      }
     end
 
     def citation_for(style)
@@ -21,32 +26,56 @@ module Emory
     private
 
       def item
-        CiteProc::Item.new(key_val_chunk_1.merge(key_val_chunk_2).merge(key_val_chunk_3))
+        CiteProc::Item.new(key_value_chunk_1.merge(key_value_chunk_2).merge(key_value_chunk_3))
       end
 
       def mla_url_test
-        [obj[:holding_repository_tesim], obj[:edition_tesim], obj[:publisher_tesim], obj[:date_issued_tesim]].any?(&:present?)
+        [
+          obj[:holding_repository_tesim],
+          obj[:edition_tesim],
+          obj[:publisher_tesim],
+          obj[:date_issued_tesim]
+        ].any?(&:present?)
       end
 
       def abnormal_chars?
-        obj[:creator_tesim]&.any? { |a| a.match(/[^A-Za-z\s]+/) }
+        obj[:creator_tesim]&.any? { |a| a.match(/[^\p{L}\s]+/) }
       end
 
-      def key_val_chunk_1
-        { id: :item, abstract: obj[:abstract_tesim]&.join(', '), archive_location: obj[:sublocation_tesim]&.join(', '), author: obj[:creator_tesim]&.join(', '),
-          "call-number": obj[:local_call_number_tesim]&.join(', '), edition: obj[:edition_tesim]&.join(', '), institution: obj[:institution_tesim]&.join(', ') }
+      def key_value_chunk_1
+        {
+          id: :item,
+          abstract: obj[:abstract_tesim]&.join(', '),
+          archive_location: obj[:sublocation_tesim]&.join(', '),
+          author: obj[:creator_tesim]&.join(', '),
+          "call-number": obj[:local_call_number_tesim]&.join(', '),
+          edition: obj[:edition_tesim]&.join(', '),
+          institution: obj[:institution_tesim]&.join(', ')
+        }
       end
 
-      def key_val_chunk_2
-        { archive: obj[:holding_repository_tesim]&.join(', '), publisher: obj[:publisher_tesim]&.join(', '), title: obj[:title_tesim]&.join(', '),
-          "collection-title": obj[:member_of_collections_ssim]&.join(', '), type: [obj[:human_readable_content_type_ssim]&.first&.downcase]&.join(', '),
-          url: url, issued: (obj[:date_issued_tesim] || obj[:year_issued_isim] || obj[:year_created_isim] || [0])&.first&.to_i }
+      def key_value_chunk_2
+        {
+          archive: obj[:holding_repository_tesim]&.join(', '),
+          publisher: obj[:publisher_tesim]&.join(', '),
+          title: obj[:title_tesim]&.join(', '),
+          "collection-title": obj[:member_of_collections_ssim]&.join(', '),
+          type: [obj[:human_readable_content_type_ssim]&.first&.downcase]&.join(', '),
+          url: url,
+          issued: (obj[:date_issued_tesim] || obj[:year_issued_isim] || obj[:year_created_isim] || [0])&.first&.to_i
+        }
       end
 
-      def key_val_chunk_3
-        { dimensions: obj[:extent_tesim]&.join(', '), event: obj[:conference_name_tesim]&.join(', '), genre: obj[:content_genres_tesim]&.join(', '),
-          ISBN: obj[:isbn_tesim]&.join(', '), ISSN: obj[:issn_tesim]&.join(', '), keyword: obj[:keywords_tesim]&.join(', '),
-          "publisher-place": obj[:place_of_production_tesim]&.join(', ') }
+      def key_value_chunk_3
+        {
+          dimensions: obj[:extent_tesim]&.join(', '),
+          event: obj[:conference_name_tesim]&.join(', '),
+          genre: obj[:content_genres_tesim]&.join(', '),
+          ISBN: obj[:isbn_tesim]&.join(', '),
+          ISSN: obj[:issn_tesim]&.join(', '),
+          keyword: obj[:keywords_tesim]&.join(', '),
+          "publisher-place": obj[:place_of_production_tesim]&.join(', ')
+        }
       end
   end
 end
--- lib/emory/citation_string_processor.rb
+++ lib/emory/citation_string_processor.rb
@@ -2,19 +2,18 @@
 
 module CitationStringProcessor
   def url
-    return unless obj[:id]
-    "https://digital.library.emory.edu/purl/#{obj[:id]}"
+    "https://digital.library.emory.edu/purl/#{obj[:id]}" if obj[:id]
   end
 
-  def proc_mid_com(field)
+  def append_string_with_comma(field)
     "#{field&.first}, " if field.present?
   end
 
-  def proc_mid_per(field)
+  def append_string_with_period(field)
     "#{field&.first}. " if field.present?
   end
 
-  def  apa_edition
+  def apa_edition
     ", #{obj[:edition_tesim].first}" if obj[:edition_tesim].present?
   end
 
@@ -28,53 +27,66 @@ module CitationStringProcessor
     end
   end
 
-  def auth_no_per
+  def author_name_no_period
     return obj[:creator_tesim].map { |a| a.first(a.size - 1) } if obj[:creator_tesim]&.any? { |a| a&.split('')&.last == '.' }
     obj[:creator_tesim]
   end
 
-  def sanitized_apa_auth
-    return apa_creator_name unless abnormal_chars? || obj[:creator_tesim].blank?
-    "#{auth_no_per&.join(', & ')}. " unless obj[:creator_tesim].blank?
+  def formatted_apa_author
+    return joined_apa_author_names unless abnormal_chars? || obj[:creator_tesim].blank?
+    "#{author_name_no_period&.join(', & ')}. " unless obj[:creator_tesim].blank?
   end
 
-  def sanitized_mla_auth
-    return mla_creator_name unless abnormal_chars? || obj[:creator_tesim].blank?
-    "#{auth_no_per&.join(', ')}. " unless obj[:creator_tesim].blank?
+  def formatted_mla_author
+    return joined_mla_author_names unless abnormal_chars? || obj[:creator_tesim].blank?
+    "#{author_name_no_period&.join(', ')}. " unless obj[:creator_tesim].blank?
   end
 
-  def sanitized_chic_auth
-    "#{auth_no_per&.join(', ')}, " unless auth_no_per.nil?
+  def formatted_chicago_author
+    "#{author_name_no_period&.join(', ')}, " unless author_name_no_period.nil?
   end
 
-  def apa_default
+  def apa_default_citation
     [
-      sanitized_apa_auth, ("(#{obj[:date_issued_tesim]&.first})" unless obj[:date_issued_tesim].nil?),
+      formatted_apa_author,
+      ("(#{obj[:date_issued_tesim]&.first})" unless obj[:date_issued_tesim].nil?),
       ("[#{obj[:title_tesim]&.first}#{apa_edition}]. " unless obj[:title_tesim].nil?),
-      proc_mid_com(obj[:member_of_collections_ssim]), proc_mid_per(obj[:holding_repository_tesim]), url, "."
+      append_string_with_comma(obj[:member_of_collections_ssim]),
+      append_string_with_period(obj[:holding_repository_tesim]),
+      url,
+      "."
     ].join('')
   end
 
-  def chicago_default
+  def chicago_default_citation
     [
-      sanitized_chic_auth, proc_mid_com(obj[:title_tesim]),
-      proc_mid_com(obj[:date_issued_tesim]), proc_mid_com(obj[:member_of_collections_ssim]),
-      proc_mid_per(obj[:holding_repository_tesim]), url, "."
+      formatted_chicago_author,
+      append_string_with_comma(obj[:title_tesim]),
+      append_string_with_comma(obj[:date_issued_tesim]),
+      append_string_with_comma(obj[:member_of_collections_ssim]),
+      append_string_with_period(obj[:holding_repository_tesim]),
+      url,
+      "."
     ].join('')
   end
 
-  def mla_default
+  def mla_default_citation
     [
-      sanitized_mla_auth, proc_mid_per(obj[:title_tesim]), proc_mid_per(obj[:date_issued_tesim]),
-      proc_mid_per(obj[:member_of_collections_ssim]), proc_mid_per(obj[:holding_repository_tesim]), url, "."
+      formatted_mla_author,
+      append_string_with_period(obj[:title_tesim]),
+      append_string_with_period(obj[:date_issued_tesim]),
+      append_string_with_period(obj[:member_of_collections_ssim]),
+      append_string_with_period(obj[:holding_repository_tesim]),
+      url,
+      "."
     ].join('')
   end
 
-  def apa_creator_name
+  def joined_apa_author_names
     "#{obj[:creator_tesim].map { |f| f.split(' ').last + ', ' + f.split(' ').first(f.split.size - 1)&.map(&:first)&.join('. ') }&.join(', & ')}. "
   end
 
-  def mla_creator_name
+  def joined_mla_author_names
     "#{obj[:creator_tesim].map { |f| f.split(' ').last + ', ' + f.split(' ').first(f.split.size - 1).join(' ') }&.join(', ')}. "
   end
 end
