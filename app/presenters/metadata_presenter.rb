63f11cc9047cc04d8c481b30256692fc76742cd5
Max Kadel <max@curationexperts.com>
2020-04-30 17:56:28 -0400

    green, metadata_presenter refactor

--- app/presenters/metadata_presenter.rb
+++ app/presenters/metadata_presenter.rb
@@ -8,24 +8,9 @@ class MetadataPresenter
     @config = YAML.safe_load(File.open(Rails.root.join('config', 'metadata', 'metadata.yml')))
   end
 
-  def terms
+  def terms(section)
     @config = @config.symbolize_keys
-  end
-
-  def about_this_collection
-    terms
-    @about_collection = @config[:about_this_collection].symbolize_keys
-    @document.slice(*@about_collection.keys)
-  end
-
-  def view_items_in_this_collection
-    terms
-    @view_items = @config[:view_items_in_this_collection].symbolize_keys
-    @document.slice(*@view_items.keys)
-  end
-  def is_part_of
-    terms
-    @is_part_of = @config[:is_part_of].symbolize_keys
-    @document.slice(*@is_part_of.keys)
+    @section_terms = @config[section].symbolize_keys
+    @document.slice(*@section_terms.keys)
   end
 end
