a3d4a5e1a086f47c57700ede6607e7e99a9c874d
Brad Watson <brad.watson.orlando@gmail.com>
2020-05-19 13:04:25 -0500

    Breaks multiple values in date fields into separate lines #583. (#604)
    * Breaks multiple values in date fields into separate lines.
    1. additional_catalog_helper.rb: abstracts away the test and processing for making dates fall on multiple lines.
    2. _about_this_collection_block.html.erb and _metadata_block.html.erb: adds a switch to the separate line processor if a date field is detected.
    3. additional_catalog_helper_spec.rb: creates a test suite for the helpers used here and previously.
    
    * Refactors the views and helper to address change requests.

--- app/helpers/additional_catalog_helper.rb
+++ app/helpers/additional_catalog_helper.rb
@@ -3,4 +3,12 @@ module AdditionalCatalogHelper
   def purl(doc_id)
     "https://digital.library.emory.edu/purl/#{doc_id}"
   end
+
+  def field_is_for_dates?(field_name)
+    field_name.include? "date"
+  end
+
+  def dates_on_separate_lines(document, field_name)
+    document[field_name].join("<br>")
+  end
 end
