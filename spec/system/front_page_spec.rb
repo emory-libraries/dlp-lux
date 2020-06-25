ae4bab8356101ec7e7b0069bf2d020ef88bdc5f8
Brad Watson <brad.watson.orlando@gmail.com>
2020-06-25 09:10:31 -0500

    Adds description meta tage for better SEO (#621). (#622)

--- spec/system/front_page_spec.rb
+++ spec/system/front_page_spec.rb
@@ -3,10 +3,20 @@ require 'rails_helper'
 include Warden::Test::Helpers
 
 RSpec.describe 'front page', type: :system do
+  before do
+    visit '/'
+  end
+
   it 'has featured collections' do
-    visit "/"
     expect(page).to have_content 'Health Sciences Center Library Artifact Collection'
     expect(page).to have_content 'Robert Langmuir African American Photograph Collection'
     expect(page).to have_content 'Oxford College Collection of Asian Artifacts'
+  end
+
+  it 'has a meta description tag' do
+    expect(page).to have_css(
+      'meta[name="description"][content="The digital front door to the unique cultural heritage and scholarship material from Emory Libraries. Discover, view, and download images, books, and more from our campus repositories."]',
+      visible: false
+    )
   end
 end
