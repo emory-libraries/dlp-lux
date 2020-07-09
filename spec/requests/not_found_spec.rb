2212c35837c0ec5bf3b64c9e6020c60b8c1c43ba
bwatson78 <brad.watson.orlando@gmail.com>
2020-07-09 09:36:32 -0500

    Updates the verbiage on the not found page (#550).

--- spec/requests/not_found_spec.rb
+++ spec/requests/not_found_spec.rb
@@ -15,15 +15,31 @@ RSpec.describe "404 Error Custom Page", type: :request do
   it "contains the requested headers" do
     get "/1q"
 
-    expect(response.body).to include 'Page Not Available'
-    expect(response.body).to include 'Accessing Emory Digital Collections'
-    expect(response.body).to include 'mailto:rose.library@emory.edu'
-    expect(response.body).to include 'our wiki site'
-    expect(response.body).to include 'https://wiki.service.emory.edu/display/DLPP/Emory+Digital+Collections+User+Guide'
+    expect(response.body).to include 'Page Not Found'
+    expect(response.body).not_to include 'Accessing Emory Digital Collections'
+    expect(response.body).not_to include 'mailto:rose.library@emory.edu'
+    expect(response.body).not_to include 'our wiki site'
+    expect(response.body).not_to include 'https://wiki.service.emory.edu/display/DLPP/Emory+Digital+Collections+User+Guide'
+  end
+
+  it "contains the requested links" do
+    get "/1q"
+
+    expect(response.body).not_to include 'mailto:rose.library@emory.edu'
+    expect(response.body).not_to include 'our wiki site'
+    expect(response.body).not_to include 'https://wiki.service.emory.edu/display/DLPP/Emory+Digital+Collections+User+Guide'
   end
 
   it "loads the page for unlogged in users looking for bookmarks" do
     get "/bookmarks"
     expect(response.status).to eq 404
+  end
+
+  it 'contains the right verbiage' do
+    get "/1q"
+
+    expect(response.body).to include 'You may have reached this page due to an incorrect address.'
+    expect(response.body).to include "Use your browser's back button or return"
+    expect(response.body).to include 'to continue. For more assistance,'
   end
 end
