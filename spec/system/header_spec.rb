a0104b18e252177c08a0e974290388cb293b4a94
bwatson78 <brad.watson.orlando@gmail.com>
2020-07-10 11:06:30 -0500

    Corrects the links on the header (#633).

--- spec/system/header_spec.rb
+++ spec/system/header_spec.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 require 'rails_helper'
-include Warden::Test::Helpers
 
 RSpec.describe 'header', type: :system do
+  before { visit '/' }
+
   it 'has expected css' do
-    visit "/"
-    expect(page).to have_css ".branding-header"
-    expect(page).to have_link "Advanced Search"
-    expect(page).to have_css ".search-query-form"
-    expect(page).to have_link "Emory Libraries"
+    expect(page).to have_css '.branding-header'
+    expect(page).to have_css '.search-query-form'
+    expect(page).to have_css("img[src*='shield']")
+  end
+
+  it 'has expected links' do
+    expect(page).to have_link 'Advanced Search'
+    expect(page).to have_link('Emory Libraries', href: 'https://libraries.emory.edu/')
+    link = find(:xpath, "//div[@class='branding-header logo']//a")
+    expect(link[:href]).to eq('/')
+    expect(page).to have_link('Emory University', href: 'https://emory.edu/')
   end
 end
