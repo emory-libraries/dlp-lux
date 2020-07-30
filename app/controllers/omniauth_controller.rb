4252ebb6255a994693f68824ca43e84a695f2f66
bwatson78 <brad.watson.orlando@gmail.com>
2020-07-30 11:29:48 -0400

    Switches variable environment to the catalog controller (#191).

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -3,8 +3,6 @@ class OmniauthController < Devise::SessionsController
   def new
     # Rails.logger.debug "SessionsController#new: request.referer = #{request.referer}"
     if Rails.env.production?
-      Rails.logger.debug "OmniauthController#new: request.url: #{request.url}"
-      session[:requested_page] = request.url
       redirect_to user_shibboleth_omniauth_authorize_path
     else
       super
