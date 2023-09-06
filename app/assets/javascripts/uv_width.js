f8fa8c957228abdba9085dabc418402fd5e0caee
bwatson78 <brad.watson.orlando@gmail.com>
2023-09-06 15:17:29 -0500

    Remove second batch of unnecessary files.

--- app/assets/javascripts/uv_width.js
+++
@@ -1,7 +0,0 @@
-$(document).ready( function() {
-  $('.universal-viewer-iframe').width($('.uv-container').width())
-
-  $(window).on('resize', function(){
-    $('.universal-viewer-iframe').width($('.uv-container').width())
-  })
-})
