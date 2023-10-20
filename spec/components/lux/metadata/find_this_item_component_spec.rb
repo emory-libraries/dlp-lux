b3e0117564b172e7e78f972ccce38ec17f9f7609
bwatson78 <brad.watson.orlando@gmail.com>
2023-10-20 08:10:08 -0500

    Adds test for first_column and some minor tweaks.

--- spec/components/lux/metadata/find_this_item_component_spec.rb
+++ spec/components/lux/metadata/find_this_item_component_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Lux::Metadata::FindThisItemComponent, type: :component do
     end
   end
   let(:instance) { described_class.new(document: doc) }
-  let(:doc) { SolrDocument.new(CURATE_GENERIC_WORK) }
+  let(:doc) { SolrDocument.new(doc_type) }
   let(:doc_presenter) { Blacklight::ShowPresenter.new(doc, controller.view_context) }
   let(:fields) do
     ::MetadataPresenter.new(document: doc_presenter.fields_to_render).terms(:find_this_item)
@@ -23,25 +23,70 @@ RSpec.describe Lux::Metadata::FindThisItemComponent, type: :component do
     allow(response).to receive(:[]).with('highlighting').and_return(nil)
   end
 
-  it 'has the expected labels' do
-    with_controller_class CatalogController do
-      expect(render.css('.card-body dl dt').size).to eq(section_config.size)
-      section_config.values.each do |value|
-        expect(render.css('.card-body dl dt').text).to include(value)
+  context 'for a work' do
+    let(:doc_type) { CURATE_GENERIC_WORK }
+
+    it 'has the right card title' do
+      with_controller_class(CatalogController) { expect(render.css('.card-header').text).to include('Find This Item') }
+    end
+
+    it 'has the expected labels' do
+      with_controller_class CatalogController do
+        expect(render.css('.card-body dl dt').size).to eq(section_config.size)
+        section_config.values.each do |value|
+          expect(render.css('.card-body dl dt').text).to include(value)
+        end
+      end
+    end
+
+    it 'has the right label/value per row' do
+      with_controller_class CatalogController do
+        section_config.each do |solr_field, label|
+          expect(render.css("dt.blacklight-#{solr_field.parameterize}").text).to include(label)
+          if doc[solr_field].is_a?(Array)
+            doc[solr_field].each do |value|
+              expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include(value)
+            end
+          else
+            expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include(doc[solr_field])
+          end
+        end
       end
     end
   end
 
-  it 'has the right label/value per row' do
-    with_controller_class CatalogController do
-      section_config.each do |solr_field, label|
-        expect(render.css("dt.blacklight-#{solr_field.parameterize}").text).to include(label)
-        if doc[solr_field].is_a?(Array)
-          doc[solr_field].each do |value|
-            expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include(value)
+  context 'for a collection' do
+    let(:doc_type) { COLLECTION }
+    let(:section_config) do
+      YAML.safe_load(
+        File.open(Rails.root.join('config', 'metadata', 'find_this_item.yml'))
+      ).except("system_of_record_ID_tesim", "other_identifiers_tesim", "sublocation_tesim")
+    end
+
+    it 'has the right card title' do
+      with_controller_class(CatalogController) { expect(render.css('.card-header').text).to include('Find This Collection') }
+    end
+
+    it 'has the expected labels' do
+      with_controller_class CatalogController do
+        expect(render.css('.card-body dl dt').size).to eq(section_config.size)
+        section_config.values.each do |value|
+          expect(render.css('.card-body dl dt').text).to include(value)
+        end
+      end
+    end
+
+    it 'has the right label/value per row' do
+      with_controller_class CatalogController do
+        section_config.each do |solr_field, label|
+          expect(render.css("dt.blacklight-#{solr_field.parameterize}").text).to include(label)
+          if doc[solr_field].is_a?(Array)
+            doc[solr_field].each do |value|
+              expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include(value)
+            end
+          else
+            expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include(doc[solr_field])
           end
-        else
-          expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include(doc[solr_field])
         end
       end
     end
