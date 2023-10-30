eb6878f7454bc29b5d8368e3a4c0b93ff50d3402
bwatson78 <brad.watson.orlando@gmail.com>
2023-10-30 10:30:42 -0500

    Adds test for generic_metadata and some refactoring.

--- spec/components/lux/metadata/about_this_collection_component_spec.rb
+++ spec/components/lux/metadata/about_this_collection_component_spec.rb
@@ -2,18 +2,11 @@
 require 'rails_helper'
 
 RSpec.describe Lux::Metadata::AboutThisCollectionComponent, type: :component do
-  subject(:render) do
-    with_request_url "/catalog/#{doc.id}" do
-      render_inline(instance)
-    end
-  end
-  let(:instance) { described_class.new(document: doc) }
+  include_context('setup common component variables')
   let(:doc) { SolrDocument.new(COLLECTION) }
-  let(:doc_presenter) { Blacklight::ShowPresenter.new(doc, controller.view_context) }
   let(:fields) do
     ::MetadataPresenter.new(document: doc_presenter.fields_to_render).terms(:about_this_collection)
   end
-  let(:response) { instance_double(Blacklight::Solr::Response) }
   let(:section_config) { YAML.safe_load(File.open(Rails.root.join('config', 'metadata', 'about_this_collection.yml'))) }
   let(:pulled_linked_element_classes) do
     render.css('.row dd a').map { |el| el.parent.attributes['class'].value.split(' ').first }
@@ -23,41 +16,6 @@ RSpec.describe Lux::Metadata::AboutThisCollectionComponent, type: :component do
      "blacklight-finding_aid_link_ssm"]
   end
 
-  before do
-    allow(instance).to receive(:document_presenter).and_return(doc_presenter)
-    allow(instance).to receive(:fields).and_return(fields)
-    allow(doc).to receive(:response).and_return(response)
-    allow(response).to receive(:[]).with('highlighting').and_return(nil)
-  end
-
-  it 'has the expected labels' do
-    with_controller_class CatalogController do
-      expect(render.css('.row dt').size).to eq(section_config.size)
-      section_config.values.each do |value|
-        expect(render.css('.row dt').text).to include(value)
-      end
-    end
-  end
-
-  it 'has the right label/value per row' do
-    with_controller_class CatalogController do
-      section_config.each do |solr_field, label|
-        expect(render.css("dt.blacklight-#{solr_field.parameterize}").text).to include(label)
-        doc[solr_field].each do |value|
-          if solr_field == 'finding_aid_link_ssm'
-            expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include('Finding Aid')
-          else
-            expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include(value)
-          end
-        end
-      end
-    end
-  end
-
-  it 'has the expected amount of links versus normal text' do
-    with_controller_class CatalogController do
-      expect(render.css('.row dd a').size).to eq(4)
-      expect(pulled_linked_element_classes).to match_array(expected_linked_element_classes)
-    end
-  end
+  include_examples('tests for expected component labels and values')
+  include_examples('tests for expected component link values')
 end
--- spec/components/lux/metadata/find_this_item_component_spec.rb
+++ spec/components/lux/metadata/find_this_item_component_spec.rb
@@ -2,26 +2,12 @@
 require 'rails_helper'
 
 RSpec.describe Lux::Metadata::FindThisItemComponent, type: :component do
-  subject(:render) do
-    with_request_url "/catalog/#{doc.id}" do
-      render_inline(instance)
-    end
-  end
-  let(:instance) { described_class.new(document: doc) }
+  include_context('setup common component variables')
   let(:doc) { SolrDocument.new(doc_type) }
-  let(:doc_presenter) { Blacklight::ShowPresenter.new(doc, controller.view_context) }
   let(:fields) do
     ::MetadataPresenter.new(document: doc_presenter.fields_to_render).terms(:find_this_item)
   end
-  let(:response) { instance_double(Blacklight::Solr::Response) }
   let(:section_config) { YAML.safe_load(File.open(Rails.root.join('config', 'metadata', 'find_this_item.yml'))) }
-
-  before do
-    allow(instance).to receive(:document_presenter).and_return(doc_presenter)
-    allow(instance).to receive(:fields).and_return(fields)
-    allow(doc).to receive(:response).and_return(response)
-    allow(response).to receive(:[]).with('highlighting').and_return(nil)
-  end
 
   context 'for a work' do
     let(:doc_type) { CURATE_GENERIC_WORK }
@@ -30,29 +16,7 @@ RSpec.describe Lux::Metadata::FindThisItemComponent, type: :component do
       with_controller_class(CatalogController) { expect(render.css('.card-header').text).to include('Find This Item') }
     end
 
-    it 'has the expected labels' do
-      with_controller_class CatalogController do
-        expect(render.css('.card-body dl dt').size).to eq(section_config.size)
-        section_config.values.each do |value|
-          expect(render.css('.card-body dl dt').text).to include(value)
-        end
-      end
-    end
-
-    it 'has the right label/value per row' do
-      with_controller_class CatalogController do
-        section_config.each do |solr_field, label|
-          expect(render.css("dt.blacklight-#{solr_field.parameterize}").text).to include(label)
-          if doc[solr_field].is_a?(Array)
-            doc[solr_field].each do |value|
-              expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include(value)
-            end
-          else
-            expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include(doc[solr_field])
-          end
-        end
-      end
-    end
+    include_examples('tests for expected component labels and values', '.card-body dl dt')
   end
 
   context 'for a collection' do
@@ -67,28 +31,6 @@ RSpec.describe Lux::Metadata::FindThisItemComponent, type: :component do
       with_controller_class(CatalogController) { expect(render.css('.card-header').text).to include('Find This Collection') }
     end
 
-    it 'has the expected labels' do
-      with_controller_class CatalogController do
-        expect(render.css('.card-body dl dt').size).to eq(section_config.size)
-        section_config.values.each do |value|
-          expect(render.css('.card-body dl dt').text).to include(value)
-        end
-      end
-    end
-
-    it 'has the right label/value per row' do
-      with_controller_class CatalogController do
-        section_config.each do |solr_field, label|
-          expect(render.css("dt.blacklight-#{solr_field.parameterize}").text).to include(label)
-          if doc[solr_field].is_a?(Array)
-            doc[solr_field].each do |value|
-              expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include(value)
-            end
-          else
-            expect(render.css("dd.blacklight-#{solr_field.parameterize}").text).to include(doc[solr_field])
-          end
-        end
-      end
-    end
+    include_examples('tests for expected component labels and values', '.card-body dl dt')
   end
 end
