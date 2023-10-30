5ef004df6bf84c0107e87a32ce9c4b71c72f335f
bwatson78 <brad.watson.orlando@gmail.com>
2023-10-30 11:58:48 -0500

    Adds test for is_part_of_component.

---
+++ spec/components/lux/metadata/is_part_of_component_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Lux::Metadata::IsPartOfComponent, type: :component do
+  include_context('setup common component variables')
+  let(:doc) { SolrDocument.new(doc_type) }
+  let(:fields) do
+    ::MetadataPresenter.new(document: doc_presenter.fields_to_render).terms(:is_part_of)
+  end
+  let(:section_config) { YAML.safe_load(File.open(Rails.root.join('config', 'metadata', 'is_part_of.yml'))) }
+
+  context 'for a work' do
+    let(:doc_type) { CURATE_GENERIC_WORK }
+
+    it 'has the right google tag values' do
+      with_controller_class(CatalogController) do
+        # holding_repository_view gtag
+        expect(render.css('script').text).to include("gtag('event', \"Oxford College Library\"")
+        expect(render.css('script').text).to include("'event_label': 'The Title of my Work'")
+      end
+    end
+
+    it 'has the right link value' do
+      with_controller_class(CatalogController) do
+        expect(render).to have_link(
+          'Chester W. Topp collection of Victorian yellowbacks and paperbacks',
+          href: '805fbg79d6-cor'
+        )
+      end
+    end
+  end
+
+  context 'for a collection' do
+    let(:doc_type) { COLLECTION }
+
+    it 'has the right link value' do
+      with_controller_class(CatalogController) do
+        expect(render).to have_link(
+          'A pretend Parent collection for the Yellowbacks Collection',
+          href: '805fbg79d6-cor'
+        )
+      end
+    end
+
+    it 'has the right google tag values' do
+      with_controller_class(CatalogController) do
+        # holding_repository_view gtag
+        expect(render.css('script').text).to include("gtag('event', \"Stuart A. Rose Manuscript, Archives, and Rare Book Library\"")
+        expect(render.css('script').text).to include("'event_label': 'Chester W. Topp collection of Victorian yellowbacks and paperbacks'")
+      end
+    end
+  end
+end
