94ac6e988af432be478cee99ce710e3f61046cd1
bwatson78 <brad.watson.orlando@gmail.com>
2023-10-31 08:21:39 -0500

    Adds test for second_column and a bit of refactoring.

--- spec/components/lux/metadata/first_column_component_spec.rb
+++ spec/components/lux/metadata/first_column_component_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
+# Note: This component has no rspec associated to it because of the partial call
+#   in the html requiring Warden helpers that aren't available in Component specs.
 
 RSpec.describe Lux::Metadata::FirstColumnComponent, type: :component do
   # stub variables without presenter options
@@ -15,20 +17,7 @@ RSpec.describe Lux::Metadata::FirstColumnComponent, type: :component do
     end
 
     it 'calls the right components' do
-      with_controller_class CatalogController do
-        expect(::Lux::Metadata::IsPartOfComponent).to receive(:new).with(document: doc)
-        expect(::Lux::Metadata::FindThisItemComponent).to receive(:new).with(document: doc)
-        expect(::Lux::Metadata::GenericMetadataComponent).to receive(:new).with(
-          document: doc,
-          presenter_klass: ::RelatedMaterialPresenter,
-          presenter_container_class: 'related-material',
-          title: 'Related Material',
-          add_class_dt: 'col-md-12',
-          add_class_dd: 'col-md-12'
-        )
-
-        render
-      end
+      run_components_call_tests
     end
   end
 
@@ -36,24 +25,28 @@ RSpec.describe Lux::Metadata::FirstColumnComponent, type: :component do
     let(:doc_type) { COLLECTION }
 
     describe '#this_is_collection' do
-      it('returns false') { expect(instance.this_is_collection).to be_truthy }
+      it('returns true') { expect(instance.this_is_collection).to be_truthy }
     end
 
     it 'calls the right components' do
-      with_controller_class CatalogController do
-        expect(::Lux::Metadata::IsPartOfComponent).to receive(:new).with(document: doc)
-        expect(::Lux::Metadata::FindThisItemComponent).to receive(:new).with(document: doc)
-        expect(::Lux::Metadata::GenericMetadataComponent).to receive(:new).with(
-          document: doc,
-          presenter_klass: ::RelatedMaterialPresenter,
-          presenter_container_class: 'related-material',
-          title: 'Related Material',
-          add_class_dt: 'col-md-12',
-          add_class_dd: 'col-md-12'
-        )
+      run_components_call_tests
+    end
+  end
 
-        render
-      end
+  def run_components_call_tests
+    with_controller_class CatalogController do
+      expect(::Lux::Metadata::IsPartOfComponent).to receive(:new).with(document: doc)
+      expect(::Lux::Metadata::FindThisItemComponent).to receive(:new).with(document: doc)
+      expect(::Lux::Metadata::GenericMetadataComponent).to receive(:new).with(
+        document: doc,
+        presenter_klass: ::RelatedMaterialPresenter,
+        presenter_container_class: 'related-material',
+        title: 'Related Material',
+        add_class_dt: 'col-md-12',
+        add_class_dd: 'col-md-12'
+      )
+
+      render
     end
   end
 end
