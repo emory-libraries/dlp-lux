29444a09b735acb2835bb2690dd6635652c3ad6a
bwatson78 <brad.watson.orlando@gmail.com>
2023-10-31 09:54:01 -0500

    Adds test for view_items_in_collection.

---
+++ spec/components/lux/metadata/view_items_in_collection_component_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Lux::Metadata::ViewItemsInCollectionComponent, type: :component do
+  include_context('setup common component variables', false)
+  let(:doc) { SolrDocument.new(COLLECTION) }
+
+  it 'has a value in instance variable' do
+    expect(instance.formatted_title).to be_present
+  end
+
+  it 'has a link with a solr search using collection title' do
+    expect(render).to have_link(
+      'View items in this digital collection',
+      href: '/?f%5Bsource_collection_title_ssim%5D%5B%5D=Chester+W.+Topp+collection+of+Victorian+yellowbacks+and+paperbacks&per_page=10'
+    )
+  end
+end
