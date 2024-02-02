a65c18cdd0d3f49c38994404fce2753f011cb439
bwatson78 <brad.watson.orlando@gmail.com>
2024-02-02 08:46:01 -0600

    Makes comment of lack of testing for Blacklight::MetadataFieldLayoutComponent.

--- app/components/lux/metadata_field_layout_component.rb
+++ app/components/lux/metadata_field_layout_component.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-# Blacklight v7.33.1 Override - changes the label and value classes to what we expect
+# Blacklight v7.33.1 Override - changes the label and value classes to what we expect.
+#   There is no spec written for this component in Blacklight.
 
 class Lux::MetadataFieldLayoutComponent < Blacklight::MetadataFieldLayoutComponent
   # @param field [Blacklight::FieldPresenter]
