055ab39c13bd6b80a6e096034ecd07e4a5b186f6
bwatson78 <brad.watson.orlando@gmail.com>
2024-02-02 08:39:55 -0600

    Adds tests for Lux::MetadataFieldComponent and Blacklight test helpers.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,4 +69,5 @@ RSpec.configure do |config|
   config.include ViewComponent::TestHelpers, type: :component
   config.include ViewComponent::SystemTestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
+  config.include LuxViewComponentTestHelpers, type: :component
 end
