a46079e4a5a91ae15cf233cd5a1de47c2c699f60
bwatson78 <brad.watson.orlando@gmail.com>
2025-11-24 12:09:14 -0600

    tweaking 2.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,6 @@ require 'addressable/uri'
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000', "#{Addressable::URI.parse(ENV['THUMBNAIL_URL']).host}"
-    resource '*', headers: :any, methods: methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
