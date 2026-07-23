4ed55062ab1d100e66529ef9fe97094bf03711ca
Brad Watson <brad.watson.orlando@gmail.com>
2026-07-23 15:05:12 -0500

    Update the EC2 names we use for the EC2 IP lookup tool. (#816)

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -4,4 +4,4 @@ include Ec2Ipv4Retriever
 
 set :stage, :TEST
 set :honeybadger_env, "Lux-Test"
-server find_ip_by_ec2_name(ec2_name: 'digital-test.library.emory.edu') || ENV['TEST_SERVER_IP'], user: 'deploy', roles: %i[web app db]
+server find_ip_by_ec2_name(ec2_name: 'digital-webserver-test') || ENV['TEST_SERVER_IP'], user: 'deploy', roles: %i[web app db]
