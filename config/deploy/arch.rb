91c00ea6916a780a1a8e5cdaef54aca0b704ac27
bwatson78 <brad.watson.orlando@gmail.com>
2026-07-23 14:57:50 -0500

    Update the EC2 names we use for the EC2 IP lookup tool.

--- config/deploy/arch.rb
+++ config/deploy/arch.rb
@@ -4,4 +4,4 @@ include Ec2Ipv4Retriever
 
 set :stage, :ARCH
 set :honeybadger_env, "Lux-Arch"
-server find_ip_by_ec2_name(ec2_name: 'digital-arch.library.emory.edu') || ENV['ARCH_SERVER_IP'], user: 'deploy', roles: %i[web app db]
+server find_ip_by_ec2_name(ec2_name: 'digital-webserver-arch') || ENV['ARCH_SERVER_IP'], user: 'deploy', roles: %i[web app db]
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,4 @@ include Ec2Ipv4Retriever
 
 set :stage, :PROD
 set :honeybadger_env, "Lux-Prod"
-server find_ip_by_ec2_name(ec2_name: 'digital.library.emory.edu') || ENV['PROD_SERVER_IP'], user: 'deploy', roles: %i[web app db]
+server find_ip_by_ec2_name(ec2_name: 'digital-webserver-prod') || ENV['PROD_SERVER_IP'], user: 'deploy', roles: %i[web app db]
